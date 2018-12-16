e01829827a96c469c73b15dac3217da86645214d
Unknown <zsofiaradai@gmail.com>
2018-12-16 21:15:13 +0100

    add pager service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieStorageService } from './movie/movie-storage.service';
 import { MovieService } from './movie/movie.service';
+import { PagerService } from './util/pager.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [MovieStorageService, MovieService]
+  providers: [MovieStorageService, MovieService, PagerService]
 })
 export class AppComponent {
 }

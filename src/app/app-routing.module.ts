9318f3aa4776aa20039c99d133e186a938a4397a
Unknown <zsofiaradai@gmail.com>
2018-12-16 21:15:02 +0100

    add home component as home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { MovieGridComponent } from './movie/movie-grid/movie-grid.component';
 import { MovieDetailComponent } from './movie/movie-detail/movie-detail.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-    {path: '', component: MovieGridComponent},
+    {path: '', component: HomeComponent},
     {path: 'search', component: MovieGridComponent},
     {path: 'movie-details', component: MovieDetailComponent},
     {path: 'movie-details/movie/:id', component: MovieDetailComponent},

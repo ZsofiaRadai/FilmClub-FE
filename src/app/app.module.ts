4d5a7865fb05abcd32518804b67beb70d450a5cf
Unknown <zsofiaradai@gmail.com>
2018-12-31 09:15:00 +0100

    include jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import './jquery';
+import 'bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
